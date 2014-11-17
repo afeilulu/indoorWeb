9a7b49c7ae46d443e32336fa73753aca4e982b5d
afeilulu <afeilulu@gmail.com>
2014-11-17 15:42:42 +0800

    update instead of create-drop

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -23,7 +23,7 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
             url = "jdbc:mysql://chinaairdome.com/indoor_dev?useUnicode=true&characterEncoding=UTF-8&autoReconnect=true&autoReconnectForPools=true&is-connection-validation-required=true"
 //            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
         }
